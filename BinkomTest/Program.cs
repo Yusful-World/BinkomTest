53cd4b91687249d4d1cb23e4e917a58320648818
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2025-01-23 10:33:46 +0100

    Add project files.

---
+++ BinkomTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace BinkomTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = "John Doe";
+            int age = 25;
+            bool isAdmin = true;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Admin: " + isAdmin);
+
+            IsEven(25);  //prints Odd
+
+            PrintNumbers();
+
+            Array();
+
+            Greet("Alice");
+            
+        }
+
+        public static void IsEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+
+        public static void PrintNumbers()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void Array()
+        {
+            int[] evenNumbers = new int[5] { 2, 4, 6, 8, 10 };
+            foreach (int number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static void Greet(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
